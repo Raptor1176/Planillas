ba5e4987b6dd3bd653e6c862c2b209661916ff96
Raptor <Raptor@Raptor-PC>
2018-04-21 20:43:11 -0300

    al fin salieron

--- scripts/completar.js
+++ scripts/completar.js
@@ -1,13 +1,13 @@
 function completar(IDanchor){
     
-  //  datos= JSON.parse('{"player": "Raptor","name"  : "Raptor","description": {"gender": "male","age": 30,"peso": 60,"altura": 1.87,"eyes": "black","hair": "black","origin": ""},"rekt": {}}');
-    document.getElementById("contenido").appendChild(document.createTextNode("asgasgasagasg"));
-    //par -> cabecera / contenido
+    datos= JSON.parse('{"player": "Raptor","name"  : "Raptor","description": {"gender": "male","age": 30,"peso": 60,"altura": 1.87,"eyes": "black","hair": "black","origin": ""},"rekt": {}}');
+    
+    // par -> cabecera / contenido
     par= document.createElement("div");
-   // par.setAttribute("class", "card");
+    par.setAttribute("class", "card");
         
     cabecera= document.createElement("div");
-    /* cabecera.setAttribute("class", "card-header");
+    cabecera.setAttribute("class", "card-header mb-0");
     cabeceraB= document.createElement("button");
     cabeceraB.setAttribute("id", "personaje1");
     cabeceraB.setAttribute("class", "btn btn-link");
@@ -15,17 +15,16 @@ function completar(IDanchor){
     cabeceraB.setAttribute("data-target","#personaje2");
     cabeceraB.setAttribute("aria-expanded","true");
     cabeceraB.setAttribute("aria-controls","name");
-    cabeceraB.setAttribute(document.createTextNode("PERSONAJE"));
-    */
-    cuerpo= document.createElement("div");
-    /*
+    cabeceraB.appendChild(document.createTextNode("PERSONAJE"));
+    
+    cuerpo= document.createElement("div");    
     cuerpo.setAttribute("id", "personaje2");
     cuerpo.setAttribute("class","collapse show");
     cuerpo.setAttribute("aria-labelledby","personaje1");
     cuerpo.setAttribute("data-parent","#"+ IDanchor);
-    cuerpo.setAttribute(document.createTextNode("asdasdasdsad"));
-    */
-    //cabecera.appendChild(cabeceraB);
+    cuerpo.appendChild(document.createTextNode("asdasdasdsad"));
+    
+    cabecera.appendChild(cabeceraB);
     par.appendChild(cabecera);
     par.appendChild(cuerpo);
     
